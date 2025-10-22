972ae9f7c8b757acaef015d0aa0c50eaaea7e938
Chris Dell <cjdell@gmail.com>
2025-10-22 17:27:44 +0100

    Add NFS mount

--- machines/services1/nfs-client.nix
+++ machines/services1/nfs-client.nix
@@ -25,4 +25,24 @@
       "x-systemd.after=network-online.target"
     ];
   };
+
+  fileSystems."/samsung-4tb/ds-games" = {
+    device = "10.47.49.22:/samsung-4tb/ds-games";
+    fsType = "nfs4"; # Use NFSv4 for better performance
+    options = [
+      "rw"
+      "hard"
+      "intr"
+      "rsize=1048576"
+      "wsize=1048576"
+      "proto=tcp" # Ensure TCP is used for NFS
+      "acregmin=3"
+      "acregmax=60"
+      "acdirmin=30"
+      "acdirmax=120" # Increase dir attribute cache time
+      "noatime"
+      "nodiratime"
+      "x-systemd.after=network-online.target"
+    ];
+  };
 }
