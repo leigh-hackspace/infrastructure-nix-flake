bb385a3710caf0ce42566f4b5bed9b5d89068970
Chris Dell <cjdell@gmail.com>
2025-11-13 23:22:18 +0000

    Home Assistant Proxy no longer use auth header

--- machines/services1/http.nix
+++ machines/services1/http.nix
@@ -238,66 +238,11 @@ in
         useACMEHost = "leighhack.org";
         addSSL = true;
 
-        extraConfig = ''
-          # Increase buffer size for large headers
-          # This is needed only if you get 'upstream sent too big header while reading response
-          # header from upstream' error when trying to access an application protected by goauthentik
-          proxy_buffers 8 16k;
-          proxy_buffer_size 32k;
-        '';
-
         locations."/" = {
           proxyPass = "http://10.3.1.30:8123";
+          recommendedProxySettings = true;
           proxyWebsockets = true;
-
-          extraConfig = ''
-            # authentik-specific config
-            auth_request        /outpost.goauthentik.io/auth/nginx;
-            error_page          401 = @goauthentik_proxy_signin;
-            auth_request_set $auth_cookie $upstream_http_set_cookie;
-            add_header Set-Cookie $auth_cookie;
-
-            # translate headers from the outposts back to the actual upstream
-            auth_request_set $authentik_username $upstream_http_x_authentik_username;
-            auth_request_set $authentik_groups $upstream_http_x_authentik_groups;
-            auth_request_set $authentik_email $upstream_http_x_authentik_email;
-            auth_request_set $authentik_name $upstream_http_x_authentik_name;
-            auth_request_set $authentik_uid $upstream_http_x_authentik_uid;
-
-            proxy_set_header X-authentik-username $authentik_username;
-            proxy_set_header X-authentik-groups $authentik_groups;
-            proxy_set_header X-authentik-email $authentik_email;
-            proxy_set_header X-authentik-name $authentik_name;
-            proxy_set_header X-authentik-uid $authentik_uid;
-
-            ${CONFIG.LOCAL_NETWORK}
-          '';
-        };
-
-        # all requests to /outpost.goauthentik.io must be accessible without authentication
-        locations."/outpost.goauthentik.io" = {
-          proxyPass = "http://10.3.1.36:9000/outpost.goauthentik.io";
-
-          extraConfig = ''
-            # ensure the host of this vserver matches your external URL you've configured
-            # in authentik
-            proxy_set_header    Host $host;
-            proxy_set_header    X-Original-URL $scheme://$http_host$request_uri;
-            add_header          Set-Cookie $auth_cookie;
-            auth_request_set    $auth_cookie $upstream_http_set_cookie;
-          '';
-        };
-
-        # Special location for when the /auth endpoint returns a 401,
-        # redirect to the /start URL which initiates SSO
-        locations."@goauthentik_proxy_signin" = {
-          extraConfig = ''
-            internal;
-            add_header Set-Cookie $auth_cookie;
-            return 302 /outpost.goauthentik.io/start?rd=$request_uri;
-            # For domain level, use the below error_page to redirect to your authentik server with the full redirect path
-            # return 302 https://authentik.company/outpost.goauthentik.io/start?rd=$scheme://$http_host$request_uri;
-          '';
+          extraConfig = CONFIG.LOCAL_NETWORK;
         };
       };
 
