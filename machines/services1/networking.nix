f75b406ee61982f31e392aad3ad6e2f542e54004
Chris Dell <cjdell@gmail.com>
2025-11-27 12:17:28 +0000

    Make services1 VLAN aware

--- machines/services1/networking.nix
+++ machines/services1/networking.nix
@@ -10,15 +10,89 @@ let
   CONFIG = import ./config.nix;
 in
 {
-  # Enable networking
-  networking.networkmanager.enable = true;
+  # # Enable networking
+  # networking.networkmanager.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ 80 443 ];
-  # networking.firewall.allowedUDPPorts = [ 51820 ];
+  # # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  networking = {
+    useDHCP = false;
+    firewall.enable = false;
+    networkmanager.enable = false;
+  };
+
+  systemd.network = {
+    enable = true;
+    wait-online.enable = false;
+
+    links = {
+      "10-lan" = {
+        matchConfig = {
+          MACAddress = "c8:d3:ff:a5:be:7c";
+        };
+        linkConfig = {
+          Name = "lan";
+        };
+      };
+    };
+
+    netdevs = {
+      "11-vlan225" = {
+        netdevConfig = {
+          Kind = "vlan";
+          Name = "vlan225";
+          MACAddress = "c8:d3:ff:a5:b2:25";
+        };
+        vlanConfig.Id = 225;
+      };
+
+      # "12-vlan227" = {
+      #   netdevConfig = {
+      #     Kind = "vlan";
+      #     Name = "vlan227";
+      #     MACAddress = "c8:d3:ff:a5:b2:27";
+      #   };
+      #   vlanConfig.Id = 227;
+      # };
+    };
+
+    networks = {
+      "10-lan" = {
+        matchConfig.Name = "lan";
+        linkConfig.RequiredForOnline = "yes";
+        networkConfig = {
+          DHCP = false;
+        };
+        vlan = [
+          "vlan225"
+          # "vlan227"
+        ];
+      };
+
+      "11-vlan225" = {
+        matchConfig.Name = "vlan225";
+        networkConfig = {
+          DHCP = false;
+          Address = [
+            "10.3.1.20/24"
+            "2001:8b0:1d14:225:d::1020/64"
+          ];
+          Gateway = ["10.3.1.1"];
+          DNS = ["10.3.1.1"];
+          IPv6AcceptRA = true;
+        };
+      };
+
+      # "12-vlan227" = {
+      #   matchConfig.Name = "vlan227";
+      #   networkConfig = {
+      #     DHCP = true;
+      #     IPv6AcceptRA = true;
+      #   };
+      # };
+    };
+  };
 
   # This server handles HTTP traffic so we need to re-route to itself
   networking.extraHosts = ''
@@ -28,7 +102,7 @@ in
 
   # Enable NAT
   networking.nat.enable = true;
-  networking.nat.externalInterface = "eno1";
+  networking.nat.externalInterface = "vlan225";
   networking.nat.internalInterfaces = [ "wg0" ];
 
   networking.wireguard.interfaces = {
@@ -41,12 +115,12 @@ in
       # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
       # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
       postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.47.0.0/16 -o eno1 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.47.0.0/16 -o vlan225 -j MASQUERADE
       '';
 
       # This undoes the above command
       postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.47.0.0/16 -o eno1 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.47.0.0/16 -o vlan225 -j MASQUERADE
       '';
 
       # Path to the private key file.
