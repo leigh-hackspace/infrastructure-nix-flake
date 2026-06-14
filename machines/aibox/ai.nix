3adeabfbca019ba18cecaa78b18536acd89efd18
Chris Dell <cjdell@gmail.com>
2026-06-14 16:42:36 +0100

    Tidy

--- machines/aibox/ai.nix
+++ machines/aibox/ai.nix
@@ -1,9 +1,3 @@
-## NOTES: Get the container image...
-# cd ../llama.cpp.new
-# podman build -t llama-cpp-vulkan --target server -f .devops/vulkan.Dockerfile .
-# podman save llama-cpp-vulkan -o llama-cpp-vulkan.tar
-# sudo podman load -i llama-cpp-vulkan.tar
-
 {
   pkgs,
   lib,
@@ -30,11 +24,8 @@
 
     serviceConfig =
       let
-        # llamaCmdRocm = "${pkgs.llama-cpp-leigh-rocm}/bin/llama-server --host 127.0.0.1 --port \${PORT} -t 12";
         llamaCmdVulkan = "${pkgs.llama-cpp-leigh-vulkan}/bin/llama-server --host 127.0.0.1 --port \${PORT} -t 12";
-        # llamaCmdPodman = "${lib.getExe pkgs.podman} run --rm -v /home/leigh-admin/Projects/llama.cpp.new/models:/models:Z -p \${PORT}:8080 --device=/dev/dri/renderD128 --device=/dev/dri/card0 localhost/llama-cpp-vulkan -t 12";
         modelsPath = "/home/leigh-admin/Projects/llama.cpp.new/models";
-        # modelsPathPodman = "/models";
 
         # Native Nix structure representing the YAML config
         llamaConfig = {
@@ -62,56 +53,6 @@
         Restart = "always";
       };
   };
-
-  # # View logs with: journalctl -u podman-llama-server-8b -f
-  # virtualisation.oci-containers.containers = {
-  #   llama-server-8b = {
-  #     hostname = "llama-server-8b";
-  #     image = "llama-cpp-vulkan";
-  #     cmd = [
-  #       "-m"
-  #       "/models/nvidia_Llama-3.1-8B-UltraLong-4M-Instruct-Q6_K_L.gguf"
-  #       "-ngl"
-  #       "65"
-  #       # "--ctx-size"
-  #       # "0"
-  #       "--port"
-  #       "8080"
-  #       "--parallel"
-  #       "2"
-  #       "--slots"
-  #       "--metrics"
-  #     ];
-  #     autoStart = true;
-  #     ports = [
-  #       "8081:8080"
-  #     ];
-  #     volumes = [
-  #       "/home/leigh-admin/Projects/llama.cpp.new/models:/models:Z"
-  #     ];
-  #     environment = {
-  #       TZ = "Europe/London";
-  #     };
-  #     extraOptions = [
-  #       "--device=/dev/dri/renderD128"
-  #       "--device=/dev/dri/card0"
-  #     ];
-  #   };
-  # };
-
-  # View logs with: journalctl -u llama-server-8b -f
-  # systemd.services.llama-server-8b = {
-  #   description = "LLaMa Server ROCm";
-  #   after = [ "network.target" ];
-
-  #   # Ensure the service is started at boot
-  #   wantedBy = [ "multi-user.target" ];
-
-  #   serviceConfig = {
-  #     ExecStart = "${pkgs.llama-cpp-rocm}/bin/llama-server -m /home/leigh-admin/Projects/llama.cpp.new/models/nvidia_Llama-3.1-8B-UltraLong-4M-Instruct-Q6_K_L.gguf --host 0.0.0.0 --port 8082 -ngl 100";
-  #     Restart = "always";
-  #   };
-  # };
 
   # # View logs with: journalctl -u stable-diffusion -f
   # systemd.services.stable-diffusion = {
